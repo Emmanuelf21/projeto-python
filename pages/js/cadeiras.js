5787252aecdcbb8bdc8e9b1c4262d4b0a85e6a4f
Joao-H-Moreira <joao.ham2001@gmail.com>
2025-05-07 19:26:06 -0300

    att js

--- pages/js/cadeiras.js
+++ pages/js/cadeiras.js
@@ -1,21 +1,21 @@
 async function getData() {
     try{
-        const res = await fetch("http://127.0.0.1:8000/produtos/Cadeiras");
+        const res = await fetch("http://127.0.0.1:8000/produtos/Cadeira");
         const data = await res.json();
 
-        const container = document.querySelector(".container-cadeiras");
+        const container = document.querySelector(".container-Cadeiras");
 
-        data.forEach(cadeiras => {
+        data.forEach(Cadeiras => {
             container.innerHTML += `
             <div class="card flex-column">
-                    <img src="../imagens/${cadeiras.imagem}" alt="">
+                    <img src="../imagens/${Cadeiras.imagem}" alt="">
                     <div class="flex-column">
-                        <h4>${cadeiras.nome}</h4>
-                        <p>${cadeiras.descricao}</p>
-                        <p class="preco">R$ ${cadeiras.preco}</p>
+                        <h4>${Cadeiras.nome}</h4>
+                        <p>${Cadeiras.descricao}</p>
+                        <p class="preco">R$ ${Cadeiras.preco}</p>
                     </div>
                     <div class="div-btn">
-                        <button id=${cadeiras.id} class="btn-card">Adicionar ao carrinho</button>
+                        <button id=${Cadeiras.id} class="btn-card">Adicionar ao carrinho</button>
                     </div>
                 </div>
             `
