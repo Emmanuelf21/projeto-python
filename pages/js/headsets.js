96aa7447a3f83a1ed751f075d7d8231385068b60
Joao-H-Moreira <joao.ham2001@gmail.com>
2025-05-08 19:57:56 -0300

    att headsets

---
+++ pages/js/headsets.js
@@ -0,0 +1,28 @@
+async function getData() {
+    try{
+        const res = await fetch("http://127.0.0.1:8000/produtos/Headsets");
+        const data = await res.json();
+
+        const container = document.querySelector(".container-headsets");
+
+        data.forEach(headsets => {
+            container.innerHTML += `
+            <div class="card flex-column">
+                    <img src="../imagens/${headsets.imagem}" alt="">
+                    <div class="flex-column">
+                        <h4>${headsets.nome}</h4>
+                        <p>${headsets.descricao}</p>
+                        <p class="preco">R$ ${headsets.preco}</p>
+                    </div>
+                    <div class="div-btn">
+                        <button id=${headsets.id} class="btn-card">Adicionar ao carrinho</button>
+                    </div>
+                </div>
+            `
+        });
+    }catch(error){
+        console.error("Erro ao buscar dados:", error);
+    }
+}
+
+getData()
